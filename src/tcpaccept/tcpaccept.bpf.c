71e95a3f646b2b0886226636e4b5a79f2cf76dc6
wenzhiwei11 <wenzhiwei@kylinos.cn>
2023-07-20 17:11:10 +0800

    Add tcpaccept support

---
+++ src/tcpaccept/tcpaccept.bpf.c
@@ -0,0 +1,114 @@
+// SPDX-License-Identifier: (LGPL-2.1 OR BSD-2-Clause)
+// Copyright @ 2023 - Kylin
+// Author: weirongguang <weirongguang@kylinos.cn>
+//
+// Based on tcpaccept.py - 2015 Brendan Gregg
+
+#include "vmlinux.h"
+#include <bpf/bpf_core_read.h>
+#include <bpf/bpf_tracing.h>
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_endian.h>
+
+#include "tcpaccept.h"
+#include "compat.bpf.h"
+#include "maps.bpf.h"
+
+/* Define here, because there are conflicts with include files */
+#define AF_INET		2
+#define AF_INET6	10
+
+#define MAX_PORTS	1024
+
+const volatile pid_t trace_pid = 0;
+const volatile bool filter_by_port = false;
+
+struct {
+	__uint(type, BPF_MAP_TYPE_HASH);
+	__uint(max_entries, MAX_PORTS);
+	__type(key, __u16);
+	__type(value, __u16);
+} ports SEC(".maps");
+
+static __always_inline void
+tcp_ipv4_trace(void *ctx, struct sock *sk, __u32 pid, __u16 lport, __u16 dport)
+{
+	struct data_t *data4;
+
+	data4 = reserve_buf(sizeof(*data4));
+	if (!data4)
+		return;
+
+	data4->af = AF_INET;
+	data4->pid = pid;
+	data4->ip = 4;
+	BPF_CORE_READ_INTO(&data4->saddr_v4, sk, __sk_common.skc_rcv_saddr);
+	BPF_CORE_READ_INTO(&data4->daddr_v4, sk, __sk_common.skc_daddr);
+	data4->lport = lport;
+	data4->dport = dport;
+	bpf_get_current_comm(&data4->task, sizeof(data4->task));
+
+	submit_buf(ctx, data4, sizeof(*data4));
+}
+
+static __always_inline void
+tcp_ipv6_trace(void *ctx, struct sock *sk, __u32 pid, __u16 lport, __u16 dport)
+{
+	struct data_t *data6;
+
+	data6 = reserve_buf(sizeof(*data6));
+	if (!data6)
+		return;
+
+	data6->af = AF_INET6;
+	data6->pid = pid;
+	data6->ip = 6;
+	BPF_CORE_READ_INTO(&data6->saddr_v6, sk,
+			   __sk_common.skc_v6_rcv_saddr.in6_u.u6_addr32);
+	BPF_CORE_READ_INTO(&data6->daddr_v6, sk,
+			   __sk_common.skc_v6_daddr.in6_u.u6_addr32);
+	data6->lport = lport;
+	data6->dport = dport;
+	bpf_get_current_comm(&data6->task, sizeof(data6->task));
+
+	submit_buf(ctx, data6, sizeof(*data6));
+}
+
+static __always_inline int
+trace_event(void *ctx, struct sock *sk)
+{
+	__u16 *port;
+	__u32 pid = bpf_get_current_pid_tgid() >> 32;
+
+	if (trace_pid && trace_pid != pid)
+		return 0;
+
+	if (sk == NULL)
+		return 0;
+
+	if (BPF_CORE_READ_BITFIELD_PROBED(sk, sk_protocol) != IPPROTO_TCP)
+		return 0;
+
+	__u16 family = BPF_CORE_READ(sk, __sk_common.skc_family);
+	__u16 lport = BPF_CORE_READ(sk, __sk_common.skc_num);
+	__u16 dport = BPF_CORE_READ(sk, __sk_common.skc_dport);
+
+	port = bpf_map_lookup_elem(&ports, &lport);
+	if (filter_by_port && !port)
+		return 0;
+
+	if (family == AF_INET)
+		tcp_ipv4_trace(ctx, sk, pid, lport, dport);
+	else if (family == AF_INET6)
+		tcp_ipv6_trace(ctx, sk, pid, lport, dport);
+
+	return 0;
+}
+
+SEC("kretprobe/inet_csk_accept")
+int BPF_KRETPROBE(inet_csk_accept_kretprobe, struct sock *sk)
+{
+	return trace_event(ctx, sk);
+}
+
+char LICENSE[] SEC("license") = "GPL";
---
+++ src/tcpaccept/tcpaccept.c
@@ -0,0 +1,290 @@
+// SPDX-License-Identifier: (LGPL-2.1 OR BSD-2-Clause)
+// Copyright @ 2023 - Kylin
+// Author: weirongguang <weirongguang@kylinos.cn>
+//
+// Based on tcpaccept.py - 2015 Brendan Gregg
+
+#include "commons.h"
+#include "tcpaccept.h"
+#include "tcpaccept.skel.h"
+#include "btf_helpers.h"
+#include "trace_helpers.h"
+#include "compat.h"
+#include "map_helpers.h"
+#include <arpa/inet.h>
+
+static volatile sig_atomic_t exiting;
+
+const char *argp_program_version = "tcpaccept 0.1";
+const char *argp_program_bug_address = "Rongguang Wei <weirongguang@kylinos.cn>";
+const char argp_program_doc[] =
+"\nTrace TCP accepts\n"
+"\n"
+"EXAMPLES:\n"
+"    tcpaccept             # trace all TCP accepts\n"
+"    tcpaccept -t          # include timestamps\n"
+"    tcpaccept -p 181      # only trace PID 181\n"
+"    tcpaccept -P 80,81    # only trace port 80 and 81\n"
+"    tcpaccept -4          # trace IPv4 family only\n"
+"    tcpaccept -6          # trace IPv6 family only\n"
+;
+
+static const struct argp_option opts[] = {
+	{ "time", 'T', NULL, 0, "include time column on output (HH:MM:SS)" },
+	{ "timestamp", 't', NULL, 0, "include timestamp on output" },
+	{ "pid", 'p', "PID", 0, "trace this PID only" },
+	{ "port", 'P', "PORTS", 0, "comma-separated list of local ports to trace" },
+	{ "ipv4", '4', NULL, 0, "trace IPv4 family only" },
+	{ "ipv6", '6', NULL, 0, "trace IPv6 family only" },
+	{ "help", 'h', NULL, OPTION_HIDDEN, "Show this help message and exit" },
+	{}
+};
+
+static struct env {
+	bool time;
+	bool timestamp;
+	bool pid;
+	pid_t trace_pid;
+	bool port;
+	char *target_ports;
+	bool ipv4_only;
+	bool ipv6_only;
+} env;
+
+static error_t parse_arg(int key, char *arg, struct argp_state *state)
+{
+	char *port;
+
+	switch (key) {
+	case 'h':
+		argp_state_help(state, stderr, ARGP_HELP_STD_HELP);
+		break;
+	case 'T':
+		env.time = true;
+		break;
+	case 't':
+		env.timestamp = true;
+		break;
+	case 'p':
+		env.trace_pid = argp_parse_pid(key, arg, state);
+		env.pid = true;
+		break;
+	case 'P':
+		env.port = true;
+		if (!arg) {
+			warning("No ports specified\n");
+			argp_usage(state);
+		}
+		env.target_ports = strdup(arg);
+		port = strtok(arg, ",");
+		while (port) {
+			int port_num = strtol(port, NULL, 10);
+			if (errno || port_num <= 0 || port_num > 65536) {
+				warning("Invalid ports: %s\n", arg);
+				argp_usage(state);
+			}
+			port = strtok(NULL, ",");
+		}
+		break;
+	case '4':
+		env.ipv4_only = true;
+		break;
+	case '6':
+		env.ipv6_only = true;
+		break;
+	default:
+		return ARGP_ERR_UNKNOWN;
+	}
+
+	return 0;
+}
+
+static int libbpf_print_fn(enum libbpf_print_level level, const char *format,
+			   va_list args)
+{
+	if (level == LIBBPF_DEBUG)
+		return 0;
+	return vfprintf(stderr, format, args);
+}
+
+static void sig_handler(int sig)
+{
+	exiting = 1;
+}
+
+static void print_header(void)
+{
+	if (env.time)
+		printf("%-9s", "TIME");
+
+	if (env.timestamp)
+		printf("%-9s", "TIME(s)");
+
+	printf("%-7s %-12s %-2s %-16s %-5s %-16s %-5s",
+	       "PID", "COMM", "IP", "RADDR", "RPORT", "LADDR", "LPORT");
+	printf("\n");
+}
+
+static int handle_event(void *ctx, void *data, size_t data_sz)
+{
+	char time_now[16];
+	const struct data_t *event = data;
+	char src[INET6_ADDRSTRLEN], dst[INET6_ADDRSTRLEN];
+	union {
+		struct in_addr  x4;
+		struct in6_addr x6;
+	} s, d;
+
+	if (env.ipv4_only && event->af == AF_INET6)
+		return 0;
+
+	if (env.ipv6_only && event->af == AF_INET)
+		return 0;
+
+	if (event->af == AF_INET) {
+		s.x4.s_addr = event->saddr_v4;
+		d.x4.s_addr = event->daddr_v4;
+	} else if (event->af == AF_INET6) {
+		memcpy(&s.x6.s6_addr, event->saddr_v6, sizeof(s.x6.s6_addr));
+		memcpy(&d.x6.s6_addr, event->daddr_v6, sizeof(d.x6.s6_addr));
+	}
+
+	if (env.time) {
+		strftime_now(time_now, sizeof(time_now), "%H:%M:%S");
+		printf("%-8s ", time_now);
+	}
+
+	if (env.timestamp)
+		printf("%-8.3f ", time_since_start());
+
+	printf("%-7d %-12.12s %-2lld %-16s %-5d %-16s %-5d\n",
+	       event->pid,
+	       event->task,
+	       event->ip,
+	       inet_ntop(event->af, &d, dst, sizeof(dst)),
+	       ntohs(event->dport),
+	       inet_ntop(event->af, &s, src, sizeof(src)),
+	       event->lport);
+
+	return 0;
+}
+
+static void handle_lost_events(void *ctx, int cpu, __u64 lost_cnt)
+{
+	warning("Lost %llu events on CPU #%d!\n", lost_cnt, cpu);
+}
+
+static int print_events(struct bpf_buffer *buf)
+{
+	int err;
+
+	err = bpf_buffer__open(buf, handle_event, handle_lost_events, NULL);
+	if (err) {
+		warning("Failed to open ring/perf buffer: %d\n", err);
+		return err;
+	}
+
+	print_header();
+
+	while (!exiting) {
+		err = bpf_buffer__poll(buf, POLL_TIMEOUT_MS);
+		if (err < 0 && err != -EINTR) {
+			warning("Error polling ring/perf buffer: %s\n",
+				strerror(-err));
+			break;
+		}
+		/* reset err to return 0 if exiting */
+		err = 0;
+	}
+
+	return err;
+}
+
+int main(int argc, char *argv[])
+{
+	LIBBPF_OPTS(bpf_object_open_opts, open_opts);
+	static const struct argp argp = {
+		.options = opts,
+		.parser = parse_arg,
+		.doc = argp_program_doc,
+	};
+	struct tcpaccept_bpf *obj;
+	struct bpf_buffer *buf = NULL;
+	int err;
+
+	err = argp_parse(&argp, argc, argv, 0, NULL, NULL);
+	if (err)
+		return err;
+
+	if (!bpf_is_root())
+		return 1;
+
+	libbpf_set_print(libbpf_print_fn);
+
+	err = ensure_core_btf(&open_opts);
+	if (err) {
+		warning("Failed to fetch necessary BTF for CO-RE: %s\n",
+			strerror(-err));
+		return -1;
+	}
+
+	obj = tcpaccept_bpf__open_opts(&open_opts);
+	if (!obj) {
+		warning("Failed to open BPF objects\n");
+		err = 1;
+		goto cleanup;
+	}
+
+	buf = bpf_buffer__new(obj->maps.events, obj->maps.heap);
+	if (!buf) {
+		warning("Failed to create ring/perf buffer\n");
+		err = -errno;
+		goto cleanup;
+	}
+
+	if (env.pid)
+		obj->rodata->trace_pid = env.trace_pid;
+
+	obj->rodata->filter_by_port = env.port;
+
+	err = tcpaccept_bpf__load(obj);
+	if (err) {
+		warning("failed to load BPF object: %d\n", err);
+		goto cleanup;
+	}
+
+	if (env.port) {
+		int port_map_fd = bpf_map__fd(obj->maps.ports);
+		char *port = strtok(env.target_ports, ",");
+
+		while (port) {
+			int port_num = strtol(port, NULL, 10);
+
+			bpf_map_update_elem(port_map_fd, &port_num,
+					    &port_num, BPF_ANY);
+			port = strtok(NULL, ",");
+		}
+	}
+
+	err = tcpaccept_bpf__attach(obj);
+	if (err) {
+		warning("Failed to attach BPF programs: %s\n", strerror(-err));
+		goto cleanup;
+	}
+
+	if (signal(SIGINT, sig_handler) == SIG_ERR) {
+		warning("Can't set signal handler: %s\n", strerror(errno));
+		err = 1;
+		goto cleanup;
+	}
+
+	printf("Tracing accept ... Hit Ctrl-C to end\n");
+
+	err = print_events(buf);
+
+cleanup:
+	tcpaccept_bpf__destroy(obj);
+	cleanup_core_btf(&open_opts);
+
+	return err != 0;
+}
