5b92682ea708ddbe2814e754ef3689c3d5670df8
Jackie Liu <liuyun01@kylinos.cn>
2024-09-30 10:58:56 +0800

    libpf-tools/readahead: Fix attachment failure since v5.16
    This PR accounts for the rename/refactor of the following functions.
    
    __do_page_cache_readahead -> do_page_cache_ra -> page_cache_ra_order
    by torvalds/linux@8238287eadb2 and torvalds/linux@56a4d67c264e
    
    __page_cache_alloc -> filemap_alloc_folio -> filemap_alloc_folio_noprof
    by torvalds/linux@bb3c579e25e5 and torvalds/linux@b951aaff5035.
    
    @ShawnZhong
    Link: https://github.com/iovisor/bcc/commit/0b2b116f894ab7ce77ddcef7a8bcbd2ff4238fcd
    Signed-off-by: Jackie Liu <liuyun01@kylinos.cn>
    Change-Id: Ie25693f8cdcb682d03b2de5865ffee219839ceb5
    Reviewed-on: http://gerrit.kylin.com/c/ketones/+/136916

--- src/readahead/readahead.bpf.c
+++ src/readahead/readahead.bpf.c
@@ -24,7 +24,7 @@ struct {
 
 struct hist hist = {};
 
-SEC("fentry/do_page_cache_ra")
+SEC("fentry")
 int BPF_PROG(do_page_cache_ra)
 {
 	u32 pid = bpf_get_current_pid_tgid();
@@ -34,7 +34,7 @@ int BPF_PROG(do_page_cache_ra)
 	return 0;
 }
 
-SEC("kprobe/do_page_cache_ra")
+SEC("kprobe")
 int BPF_KPROBE(do_page_cache_ra_kprobe)
 {
 	u32 pid = bpf_get_current_pid_tgid();
@@ -60,13 +60,13 @@ static __always_inline int alloc_page_ret(void *key)
 	return 0;
 }
 
-SEC("fexit/__page_cache_alloc")
+SEC("fexit")
 int BPF_PROG(page_cache_alloc_ret, gfp_t gfp, struct page *page)
 {
 	return alloc_page_ret(page);
 }
 
-SEC("kretprobe/__page_cache_alloc")
+SEC("kretprobe")
 int BPF_KRETPROBE(page_cache_alloc_kretprobe, struct page *page)
 {
 	return alloc_page_ret(page);
@@ -81,6 +81,19 @@ int BPF_PROG(filemap_alloc_folio_ret, gfp_t gfp, unsigned int order,
 
 SEC("kretprobe/filemap_alloc_folio")
 int BPF_KRETPROBE(filemap_alloc_folio_kretprobe, struct folio *folio)
+{
+	return alloc_page_ret(folio);
+}
+
+SEC("fexit/filemap_alloc_folio_noprof")
+int BPF_PROG(filemap_alloc_folio_noprof_ret, gfp_t gfp, unsigned int order,
+	     struct folio *folio)
+{
+	return alloc_page_ret(folio);
+}
+
+SEC("kretprobe/filemap_alloc_folio_noprof")
+int BPF_KRETPROBE(filemap_alloc_folio_noprof_kretprobe, struct folio *folio)
 {
 	return alloc_page_ret(folio);
 }
